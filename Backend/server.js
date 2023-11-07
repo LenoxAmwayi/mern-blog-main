924cc3d27b858c3efda8919019801fc8452cab34
Lenox <107399179+LenoxAmwayi@users.noreply.github.com>
2023-11-07 13:14:59 +0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,7 @@ const app = express() ;
 
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    origin: ["http://localhost:3000", "https://cyberhat.vercel.app"],
     credentials: true,
 }))
 
